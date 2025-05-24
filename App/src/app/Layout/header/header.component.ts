26e3c4a3b19530c32d48c791f5b3223da4159044
BHieeuss <tbh.11203@gmail.com>
2025-05-25 05:27:20 +0700

    fix menu mobile

--- App/src/app/Layout/header/header.component.ts
+++ App/src/app/Layout/header/header.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMenuOpen = false;
 
+  ngOnInit(): void {
+    this.onScroll();
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollToSection(event: Event) {
+    event.preventDefault();
+    const targetId = (event.target as HTMLAnchorElement)
+      .getAttribute('href')
+      ?.substring(1);
+    const target = document.getElementById(targetId!);
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.isMenuOpen = false;
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPos = window.pageYOffset;
+    const anchors = document.querySelectorAll('.nav a');
+
+    anchors.forEach((anchor) => {
+      const href = anchor.getAttribute('href');
+      if (!href || !href.startsWith('#')) return;
+
+      const section = document.querySelector(href);
+      if (section) {
+        const top = (section as HTMLElement).offsetTop;
+        const bottom = top + (section as HTMLElement).offsetHeight;
+
+        if (scrollPos >= top && scrollPos < bottom) {
+          anchors.forEach((a) => a.classList.remove('active'));
+          anchor.classList.add('active');
+        }
+      }
+    });
+  }
 }
